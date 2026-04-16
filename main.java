bbb96f9f9e6eb63f4e148ddd90253cd8104b2629
Umang Anand <sangmisthebest22@gmail.com>
2026-04-16 14:53:42 +0530

    initial commit

--- main.java
+++ main.java
@@ -1,20 +1,13 @@
-import java.util.*;
 public class main{
     public static void main(String[]args){
-        Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt();
-        int B = sc.nextInt();
-        int C = sc.nextInt();
-
-        if ((A >= B) && (A >= C)){
-            System.out.print("A is the largest number");
-        }
-        else if ( B >= C){
-            System.out.print("B is largest ");
-        }
-        
-        else{
-            System.out.print("C : is the Largest");
+        int n= 5;
+        for(int i=1; i<=n; i++){ //This outerloop is for Making Rows 
+            for(int j=1; j<=n-i;j++){ // This inner loop is for printing "Stars"..
+                System.out.print(" ");
+            }for(int j= 1; j<2*i-1; j++){  // This loop is for printing "*"..
+                System.out.print("*");
+            }System.out.println(); //  This print statement is for printing each rows to the next line ...
         }
     }
+
 }

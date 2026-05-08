8f473590dac118a81db3d79b9069feb85061baa8
Umang Anand <sangmisthebest22@gmail.com>
2026-05-08 22:53:15 +0530

    Kadanes Algorithm

--- Array/MaxSubarraySum.java
+++ Array/MaxSubarraySum.java
@@ -1,32 +1,61 @@
-//Max SubArray Sum Brute Force Approach ///
+//// SUM OF MAXIMUM OF SUB-ARRAY USING ----------"KADANES ALGORITHM"
+
 public class MaxSubarraySum {
 
- public static void MaxSubarraySum(int numbers[]) {
+ public static void KADANES(int numbers[]) {
   int currSum = 0;
   int maxSum = Integer.MIN_VALUE;
 
   for (int i = 0; i < numbers.length; i++) {
-   int start = i;
-   for (int j = i; j < numbers.length; j++) {
-    int end = j;
+
+   currSum += numbers[i];
+   if (currSum < 0) {
     currSum = 0;
-    for (int k = start; k <= end; k++) {
-     currSum += numbers[k];
-    }
-    System.out.println(currSum);
-    if (maxSum < currSum) {
-     maxSum = currSum;
-
-    }
-
    }
+
+   maxSum = Math.max(currSum, maxSum);
+
   }
+
+  System.out.println("your maxSubarray : " + maxSum);
  }
 
  public static void main(String[] args) {
-  int numbers[] = { 2, 4, 6, 8, 10 };
+  int numbers[] = { 2, -3, 4, -6 };
 
-  MaxSubarraySum(numbers);
+  KADANES(numbers);
  }
-
 }
+
+// Max SubArray Sum Brute Force Approach ///
+// public class MaxSubarraySum {
+
+// public static void MaxSubarraySum(int numbers[]) {
+// int currSum = 0;
+// int maxSum = Integer.MIN_VALUE;
+
+// for (int i = 0; i < numbers.length; i++) {
+// int start = i;
+// for (int j = i; j < numbers.length; j++) {
+// int end = j;
+// currSum = 0;
+// for (int k = start; k <= end; k++) {
+// currSum += numbers[k];
+// }
+// System.out.println(currSum);
+// if (maxSum < currSum) {
+// maxSum = currSum;
+
+// }
+
+// }
+// }
+// }
+
+// public static void main(String[] args) {
+// int numbers[] = { 2, 4, 6, 8, 10 };
+
+// MaxSubarraySum(numbers);
+// }
+
+// }
